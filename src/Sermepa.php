57c2365514cac5e3e3605c5998a2a8ba86221d8a
Manuel Egío <manu@facine.es>
2020-10-15 23:04:27 +0200

    Issue #16: Fix Authentication protocol EMV3DS missing error message

--- src/Sermepa.php
+++ src/Sermepa.php
@@ -450,7 +450,7 @@ class Sermepa implements SermepaInterface {
     );
 
     return array_filter($parameters, function($parameter) {
-      return ($parameter !== NULL && $parameter !== FALSE && $parameter !== '');
+      return ($parameter !== NULL && $parameter !== FALSE && $parameter !== '' && !empty($parameter)  );
     });
   }
 
